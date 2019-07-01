d64e344603749a3972bfbb32bfc056b514def21e
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2019-07-01 16:50:07 -0500

    Commit 2

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import Project from '../tools/Project';
+import projectsData from '../data/projects.json';
+
 const Projects = ({ windowWidth }) => {
+  console.log(projectsData);
   return (
-    <section className='projects'>
+    <section className="projects">
       <h4>Check Out Some Of My Work</h4>
-      <div className='projects-wrap'>
-        <Project direction='left' windowWidth={windowWidth} />
-        <Project direction='right' windowWidth={windowWidth} />
+      <div className="projects-wrap">
+        {projectsData.map((project, index) =>
+          index % 2 === 0 ? (
+            <Project
+              key={index}
+              direction="left"
+              windowWidth={windowWidth}
+              project={project}
+            />
+          ) : (
+            <Project
+              key={index}
+              direction="right"
+              windowWidth={windowWidth}
+              project={project}
+            />
+          )
+        )}
       </div>
     </section>
   );
