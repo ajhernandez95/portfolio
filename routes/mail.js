694ca6081c39905c049660a1e1ce0329e26f9a05
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2019-07-08 23:05:47 -0500

    email set up

--- routes/mail.js
+++ routes/mail.js
@@ -1,42 +1,37 @@
 const express = require('express');
 const router = express.Router();
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 router.post('/', (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+  const transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    secureConnection: false,
+    port: 587,
+    auth: {
+      user: process.env.CONTACT_EMAIL,
+      pass: process.env.CONTACT_PASSWORD
+    },
+    tls: {
+      ciphers: 'SSLv3'
+    }
+  });
 
-  // var transporter = nodemailer.createTransport({
-  //   host: 'smtp-mail.outlook.com', // hostname
-  //   secureConnection: false, // TLS requires secureConnection to be false
-  //   port: 587, // port for secure SMTP
-  //   auth: {
-  //     user: process.env.CONTACT_EMAIL,
-  //     pass: process.env.CONTACT_PASSWORD
-  //   },
-  //   tls: {
-  //     ciphers: 'SSLv3'
-  //   }
-  // });
+  var mailOptions = {
+    from: 'Portfolio Website <alexportfolio95@outlook.com>',
+    to: 'ajhernandez95@live.com',
+    subject: `Message from:${req.body.name}`,
+    text: 'text shows?',
+    html: `${req.body.msg}`
+  };
 
-  // // setup e-mail data, even with unicode symbols
-  // var mailOptions = {
-  //   from: '<alexportfolio95@outlook.com>', // sender address (who sends)
-  //   to: 'ajhernandez95@live.com', // list of receivers (who receives)
-  //   subject: `Message from ${req.body.name}`, // Subject line
-  //   text: 'text shows?', // plaintext body
-  //   html: `${req.body.msg}` // html body
-  // };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      return console.log(error);
+    }
 
-  // // send mail with defined transport object
-  // transporter.sendMail(mailOptions, function(error, info) {
-  //   if (error) {
-  //     return console.log(error);
-  //   }
-
-  //   console.log('Message sent: ' + info.response);
-  // });
+    console.log('Message sent: ' + info.response);
+  });
 });
 
 module.exports = router;
